ad52b42a8f33060db3d77344df051fc9d38bfd03
朱迪炜 <zhudiwei_13zhfx@126.com>
2020-07-09 13:18:15 +0800

    完成员工信息VO

---
+++ src/main/java/com/ylkj/modelcal/vo/Employee.java
@@ -0,0 +1,54 @@
+package com.ylkj.modelcal.vo;
+
+import lombok.Data;
+
+/**
+ * 人员信息（项目部人员）
+ *
+ * @author Zhudiwei
+ * @date 2020/7/9
+ */
+@Data
+public class Employee {
+    /**
+     * 岗位
+     */
+    private String post;
+
+    /**
+     * 从业年限
+     */
+    private Integer workingAge;
+
+
+    /**
+     * 职业资格
+     */
+    private String proQualification;
+
+    /**
+     * 专业工作业绩
+     */
+    private String workExperience;
+
+    /**
+     * 职业资格得分
+     */
+    private Double proQualificationScore;
+
+    /**
+     * 从业年限得分
+     */
+    private Double workingAgeScore;
+
+    /**
+     * 专业工作业绩得分
+     */
+    private Double workExperienceScore;
+
+    /**
+     * 综合指标值
+     */
+    private Double totalScore;
+
+}
